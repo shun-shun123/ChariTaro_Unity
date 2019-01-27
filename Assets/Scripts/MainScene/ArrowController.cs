ff8431b8dcfd5310646b79be64cc77b5e72645d8
斉藤俊介 <saitoushunsuke@saitoushunsukenoiMac.local>
2019-01-28 07:16:12 +0900

    Circleの位置を反映させるロジックを実装

--- Assets/Scripts/MainScene/ArrowController.cs
+++ Assets/Scripts/MainScene/ArrowController.cs
@@ -7,11 +7,18 @@ namespace MainScene
 {
     public class ArrowController : MonoBehaviour
     {
+
+        #region Private Serialized Fields
+        [SerializeField]
+        GameObject circle;
+        #endregion
         #region Private Fields
         private float lastLongitude = 0.0f;
         private float lastLatitude = 0.0f;
         private float longitude;
         private float latitude;
+        private const float latPerOneMeter = 0.000008983148616f;
+        private const float longPerOneMeter = 0.000010966382364f;
         #endregion
 
         // 現在位置と登録位置の差から方角を取り出すための変数
@@ -37,6 +44,15 @@ namespace MainScene
             Vector2 diffVector = new Vector2(diffLong, diffLat);
             float angle = Vector2.Angle(toNorth, diffVector);
             transform.rotation = Quaternion.Euler(0, 0, toNorthAngle + angle);
+            circle.transform.position = transform.position + transform.up * CirclePlacement(diffLat, diffLong);
+        }
+
+        private float CirclePlacement(float diffLat, float diffLong)
+        {
+            float latMagnification = diffLat / latPerOneMeter;
+            float longMagnification = diffLong / longPerOneMeter;
+            float distance = Mathf.Sqrt(Mathf.Pow(latMagnification, 2.0f) * Mathf.Pow(longMagnification, 2.0f));
+            return distance;
         }
     }
 }
